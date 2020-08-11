d1156b85e7a8980c68b4f2eb539bd76814d23e55
Kimseonju <tjsdkdlel199@naver.com>
2020-08-11 13:41:56 +0900

    rest Community_service작성

---
+++ app/src/main/java/com/example/saojeong/rest/dto/CommunityDto.java
@@ -0,0 +1,10 @@
+package com.example.saojeong.rest.dto;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class CommunityDto {
+    @SerializedName("test")
+    @Expose
+    private String test;
+}
