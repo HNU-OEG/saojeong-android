27f7aa0d2e9159cfffb9be4891a1221a1df38a5d
plzprayme <dbfpzk142@gmail.com>
2020-09-06 18:37:48 +0900

    URL 배포 방식 변경

--- app/src/main/java/com/example/saojeong/util/StringUtils.java
+++ app/src/main/java/com/example/saojeong/util/StringUtils.java
@@ -16,6 +16,6 @@ public class StringUtils {
     }
 
     public static String getServerUrl() {
-        return String.valueOf(R.string.dev_url);
+        return "https://saojeong-dev.hnulinc.c11.kr/";
     }
 }
